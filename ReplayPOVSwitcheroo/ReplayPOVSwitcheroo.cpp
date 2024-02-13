5a6d863cca39c3613d9bcc05a2c03e1fdc5cb4a7
Adam <Adam@DESKTOP-5EARBC7>
2024-02-13 00:03:11 -0800

    Add project files.

---
+++ ReplayPOVSwitcheroo/ReplayPOVSwitcheroo.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+#include "ReplayPOVSwitcheroo.h"
+#include "bakkesmod/wrappers/includes.h"
+
+
+BAKKESMOD_PLUGIN(ReplayPOVSwitcheroo, "Replay POV Switcher", plugin_version, PLUGINTYPE_FREEPLAY)
+
+std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
+
+void ReplayPOVSwitcheroo::onLoad()
+{
+	_globalCvarManager = cvarManager;
+	LOG("Replay POV Switcher Plugin loaded!");
+
+	//Reset player goals to 0
+	gameWrapper->HookEvent("Function TAGame.Team_TA.PostBeginPlay",
+		[this](std::string eventName) {
+			playerGoals = 0;
+		});
+
+	gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode",
+		[this](std::string eventName) {
+
+			//Make sure we are in game
+			if (gameWrapper->IsInOnlineGame()) {
+				ServerWrapper server = gameWrapper->GetOnlineGame();
+
+				//Make sure there is a server
+				if (!server.IsNull()) {
+					GameSettingPlaylistWrapper playlist = server.GetPlaylist();
+
+					//Make sure it's not local
+					if (!playlist.IsLanMatch() && !playlist.IsPrivateMatch()) {
+						PlayerControllerWrapper localPrimaryPlayerController = server.GetLocalPrimaryPlayer();
+
+						//Get the primary player (you)
+						if (!localPrimaryPlayerController.IsNull()) {
+							PriWrapper localPrimaryPlayer = localPrimaryPlayerController.GetPRI();
+
+							if (playerGoals == localPrimaryPlayer.GetMatchGoals()) {
+								//Someone else scored so make the pov replay true
+								cvarManager->executeCommand("cl_goalreplay_pov 1");
+							}
+
+							else {
+								//You scored so make the pov replay false
+								cvarManager->executeCommand("cl_goalreplay_pov 0");
+								playerGoals = localPrimaryPlayer.GetMatchGoals();
+							}
+						}
+					}
+				}
+			}
+		});
+
+	//Reset player goals to 0
+	gameWrapper->HookEvent("Function TAGame.GameEvent_Soccar_TA.EventMatchEnded",
+		[this](std::string eventName) {
+			playerGoals = 0;
+		});
+
+	//Reset player goals to 0
+	gameWrapper->HookEvent("Function TAGame.GFxData_MainMenu_TA.MainMenuAdded",
+		[this](std::string eventName) {
+			playerGoals = 0;
+		});
+}
---
+++ ReplayPOVSwitcheroo/ReplayPOVSwitcheroo.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "GuiBase.h"
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
+#include "bakkesmod/wrappers/GameObject/Stats/StatEventWrapper.h"
+
+#include "version.h"
+constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
+
+
+class ReplayPOVSwitcheroo: public BakkesMod::Plugin::BakkesModPlugin
+{
+	void onLoad() override;
+
+public:
+
+	int playerGoals = 0;
+
+};
